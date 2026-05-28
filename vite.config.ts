cb0936698b6c8b0b57706065ac6b02c062b0ab0b
Fleuquer Lima <fleuquer.lima@embras.net>
2026-05-28 18:52:25 -0300

    fix: corrige base path para Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import removeConsole from 'vite-plugin-remove-console'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/git-calm/',
+  plugins: [react(), removeConsole()],
+  base: '/',
   server: {
     port: 5173,
     strictPort: false,
